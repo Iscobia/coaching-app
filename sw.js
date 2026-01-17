99b7d9e5d79ccb4bec0f1c6a2166d79227263ae2
Iscobia <Iscobia@users.noreply.github.com>
2026-01-17 23:38:04 +0100

    Update sw.js
    version sans gestion de cache : risque d'erreur sur iOS à cause des audios potentiellement trop lourds.

--- sw.js
+++ sw.js
@@ -1,40 +1,11 @@
-const CACHE_NAME = 'coaching-app-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/app.js',
-  // si tu ajoutes un fichier CSS ou images, tu peux les mettre ici
-];
-
-// Installer le service worker et mettre les fichiers en cache
 self.addEventListener('install', event => {
-  console.log('[SW] Install');
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('[SW] Mise en cache des fichiers');
-        return cache.addAll(urlsToCache);
-      })
-  );
+  console.log('Service Worker installé');
+  self.skipWaiting();
 });
 
-// Activer le service worker
 self.addEventListener('activate', event => {
-  console.log('[SW] Activate');
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      );
-    })
-  );
+  console.log('Service Worker activé');
+  self.clients.claim();
 });
 
-// Intercepter les requêtes et servir depuis le cache si disponible
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
+self.addEventListener('fetch', event => {});
