41cf91b1be225de767a14ebbacfbaffdb4aac0b4
Iscobia <Iscobia@users.noreply.github.com>
2026-01-17 23:30:54 +0100

    Create notifications.js

---
+++ notifications.js
@@ -0,0 +1,4 @@
+// Placeholder pour les notifications OneSignal (à configurer ensuite)
+console.log("notifications.js chargé");
+
+// Ici tu pourras init OneSignal et envoyer les notifications selon le programme et le jour stockés dans localStorage
